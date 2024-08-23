e9e6e166dcc9a586fe5fd83a42c566ec0841fafa
tomasr-18 <tomasrydenstam@gmail.com>
2024-08-23 15:28:36 +0200

    vet inte vart jag ska börja

--- app_3/main.py
+++ app_3/main.py
@@ -1,8 +1,24 @@
 import requests
 from google.cloud import bigquery
-#from dotenv import load_env
+from datetime import datetime,timedelta
+from dotenv import load_dotenv
+import os
+import sys
 
-def write(data,table='test_table',project_id='tomastestproject-433206',dataset='testdb_1')->None:
+
+def get_news_data(company: str, api_key: str, from_date=(datetime.now() - timedelta(days=2)).strftime('%Y-%m-%d'), to_date=datetime.now().strftime('%Y-%m-%d'), sort_by='relevance', language='en'):
+    url = f'https://newsapi.org/v2/everything?q={company}&from={from_date}&to={to_date}&sortBy={sort_by}&language={language}&apiKey={api_key}'
+    response = requests.get(url=url)
+    
+    print('from:',from_date)
+    print('to: ',to_date)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print('Misslyckad request till newsapi', response.status_code)
+
+
+def write(data, company: str, table='table_1', project_id='tomastestproject-433206', dataset='testdb_1') -> None:
 
     # Initiera BigQuery-klienten
     client = bigquery.Client.from_service_account_json(
@@ -10,39 +26,34 @@ def write(data,table='test_table',project_id='tomastestproject-433206',dataset='
 
     # Definiera din dataset och tabell
     table_id = f"{project_id}.{dataset}.{table}"
+    #print(table_id)
+    
+    rows_to_insert = []
 
-    # # Data du vill skicka (kan vara en lista med dictionaries)
-    # rows_to_insert = [
-    #     {"column1": "value1", "column2": "value2"},
-    #     {"column1": "value3", "column2": "value4"},
-    # ]
+    for row in data['articles']:
+        row['source'] = row['source']['name']
+        row['company'] = company
+        row['fetch_date'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        rows_to_insert.append(row)
+    #print(rows_to_insert)
 
-    # Använd insert_rows_json för att skicka data till tabellen
-    errors = client.insert_rows_json(table_id, data)
-
-    # Hantera eventuella fel
-    if errors == []:
-        print("Data skickades till BigQuery!")
-    else:
-        print("Följande fel uppstod:", errors)
+    errors = client.insert_rows_json(table_id, rows_to_insert)
+    print(errors)
 
 
-def get_data(api_key:str,date=None):
-    #url=f'http://api.weatherapi.com/v1/current.json?key={api_key}&q=bulk'
-    url=f'http://api.weatherapi.com/v1/forecast.json?key={api_key}&q=London&days=10&aqi=no&alerts=no'
-    response=requests.get(url=url)
-    if response.status_code==200:
-        return response.json()
-    else:
-        print('fel!')
-
-
-def main():
-    data=get_data('9a0a847704c34ec6b84124735242208')
+def main(company):
+    load_dotenv()
+    
+    data = get_news_data(
+        api_key=os.getenv('NEWS_API_KEY'), company=company)
     print(data)
-    write(data=data)
+    print(company)
+
+    write(data=data,company=company)
 
 
 
 if __name__=='__main__':
-    main()
+    company = sys.argv[1]
+    
+    main(company=company)
