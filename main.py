dce59aa3be3175d9bc465bb17bb4a699c3823898
tomasr-18 <tomasrydenstam@gmail.com>
2024-08-25 22:07:51 +0200

    news

--- main.py
+++ main.py
@@ -67,6 +67,6 @@ def fetch_news():
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+   
+    app.run(host='0.0.0.0', port=8080)
     
