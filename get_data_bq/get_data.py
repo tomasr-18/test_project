f36cd34e30fcebe078cff36bf5396add4d6a3fe5
tomasr-18 <tomasrydenstam@gmail.com>
2024-08-26 14:41:37 +0200

    asd

--- get_data_bq/get_data.py
+++ get_data_bq/get_data.py
@@ -9,29 +9,43 @@ import db_dtypes
 from dotenv import load_dotenv
 
 
+from google.cloud import bigquery
+import os
+import pandas as pd
+
+
 def fetch_data_by_date(pub_date=None, table='table_1', project_id='tomastestproject-433206', dataset='testdb_1'):
-    #load_dotenv()
-    # Ange sökvägen till din service account JSON-fil
+    # Set the path to your service account JSON file
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'get_data_bq/tomastestproject-433206-adc5bc090976.json'
 
     table_id = f"{project_id}.{dataset}.{table}"
-    # Skapa en BigQuery-klient
+    # Create a BigQuery client
     client = bigquery.Client()
 
- 
+    # Build your SQL query
     query = f"""
-        SELECT company,title,description,publishedAt
+        SELECT company, title, description, publishedAt
         FROM `{table_id}`
-        WHERE LEFT(publishedAt, 10) = '{pub_date}';
+        WHERE LEFT(publishedAt, 10) = '{pub_date}'
+    """
 
-        """
-
+    # Execute the SQL query
     query_job = client.query(query)
 
-    # Hämta resultaten
+    # Fetch the results
     results = query_job.result()
 
-    return results.to_dataframe()
+    # Convert results to a DataFrame
+    df = results.to_dataframe()
+
+    # Check if DataFrame is empty and raise an error if needed
+    if df.empty:
+        raise ValueError(f"No data found for the given date: {pub_date}")
+
+    return df
+
+
+
 
 
 #Exempel på användning av funktionen
