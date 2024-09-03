c0c977c76214d853804bc365f91053a41dfccafb
Mateusz <mwasylow@gmail.com>
2024-09-03 22:07:47 +0200

    cloudbuild

--- fetch_stocks/fetch_stocks_raw.py
+++ fetch_stocks/fetch_stocks_raw.py
@@ -132,4 +132,4 @@ async def handle_raw_stock_data(stock_request: StockRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
