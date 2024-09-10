028725dc6c42a4749ea109d09c1b5247ac7053a9
tomasr-18 <tomasrydenstam@gmail.com>
2024-09-10 09:34:47 +0200

    testar model-scripts

--- ml_model/main.py
+++ ml_model/main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 from typing import Optional , List
 import os
 from datetime import datetime, timedelta, timezone
-#import logging
+
 
 load_env_from_secret(project_id=os.getenv("PROJECT_ID"),
                      secret_name=os.getenv("SECRET_NAME_ENV"))
@@ -20,7 +20,7 @@ class ModelRequest(BaseModel):
     project_id: Optional[str] = os.getenv("PROJECT_ID")
     dataset: Optional[str] = os.getenv("DATA_SET")
     table_from: Optional[str] = 'avg_scores_and_stock_data_right'
-    prediction_table: Optional[str] = 'predictions'
+    prediction_table: Optional[str] = 'predictions_copy'
 
 # Definiera POST endpoint för att hämta, rensa och analysera nyheter
 
@@ -31,56 +31,26 @@ def train_model_endpoint(request: ModelRequest):
     if get_latest_date() != yesterday:
         return {"messege":"no data to predict from"}
     else:
-        try:
-            df = get_data_by_company(company=request.company_list,
-                                    table=request.table_from)
-        except Exception as e:
-            raise HTTPException(
-                status_code=500, detail=f"Error fetching data: {str(e)}")
+        df = get_data_by_company(company=request.company_list,
+                                 table=request.table_from)
 
-        try:
-            transformed_df = transform_data_to_model(df)
-        except Exception as e:
-            raise HTTPException(
-                status_code=500, detail=f"Error transforming data: {str(e)}")
 
-        try:
-            df_rolling_avg = calculate_rolling_average(
-                df=transformed_df, column_name="close", window_size=3)
-        except Exception as e:
-            raise HTTPException(
-                status_code=500, detail=f"Error calculating rolling average: {str(e)}")
+        transformed_df = transform_data_to_model(df)
 
-        try:
-            models, predictions_rows, date = train_model(df_rolling_avg)
-        except Exception as e:
-            raise HTTPException(
-                status_code=500, detail=f"Error training model: {str(e)}")
+        df_rolling_avg = calculate_rolling_average(
+            df=transformed_df, column_name="close", window_size=3)
 
-        try:
-            y_preds = make_prediction(models_dict=models, x_pred=predictions_rows)
-        except Exception as e:
-            raise HTTPException(
-                status_code=500, detail=f"Error making predictions: {str(e)}")
+        models, predictions_rows, date = train_model(df_rolling_avg)
 
-        try:
-            list_to_big_query = transform_predictions_for_bq(
-                predictions=y_preds, date=date)
-        except Exception as e:
-            raise HTTPException(
-                status_code=500, detail=f"Error transforming predictions for BigQuery: {str(e)}")
+        y_preds = make_prediction(models_dict=models, x_pred=predictions_rows)
 
-        try:
-            save_predictions_to_big_query(data=list_to_big_query,table="predictions_copy")
-        except Exception as e:
-            raise HTTPException(
-                status_code=500, detail=f"Error saving predictions to BigQuery: {str(e)}")
+        list_to_big_query, predicted_date = transform_predictions_for_bq(
+            predictions=y_preds, date=date)
 
-        try:
-            save_model(model_dict=models, date=date.strftime('%Y-%m-%d'))
-        except Exception as e:
-            raise HTTPException(
-                status_code=500, detail=f"Error saving model: {str(e)}")
+        save_predictions_to_big_query(
+            data=list_to_big_query, table=request.prediction_table)
+
+        save_model(model_dict=models, date=predicted_date.strftime('%Y-%m-%d'))
 
         return {"status": "success", "message": "Model training and prediction completed successfully."}
     
